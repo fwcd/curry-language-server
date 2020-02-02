56a6167cfafff2eec8639e7b1f387ca7cd0246fe
fwcd <fwcdmail@gmail.com>
2020-02-02 17:18:22 +0100

    WIP: Add curry prelude

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,11 @@
 import Curry.LanguageServer.Diagnostics
 import qualified Language.Haskell.LSP.Types as J
+import Files.CymakePath
 
 main :: IO ()
 main = do
+    curryPath <- getCymake
+    putStrLn $ "Curry-Frontend path: " ++ curryPath
     diags <- fetchDiagnostics uri
     putStrLn $ "Diagnostics: " ++ show (head diags)
     where uri = J.toNormalizedUri $ J.filePathToUri "test/resources/Test.curry"
