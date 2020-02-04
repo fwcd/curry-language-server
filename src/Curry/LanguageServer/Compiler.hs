0af73fb1464300d2c72d4fd745946294e528f807
fwcd <fwcdmail@gmail.com>
2020-02-04 19:09:42 +0100

    Find Curry module using CurryDeps

--- src/Curry/LanguageServer/Compiler.hs
+++ src/Curry/LanguageServer/Compiler.hs
@@ -1,9 +1,10 @@
 module Curry.LanguageServer.Compiler (compileCurry) where
 
 -- Curry Compiler Libraries + Dependencies
+import qualified Curry.Files.Filenames as CF
 import Curry.Base.Message as CM
+import Curry.Base.Monad (CYIO, runCYIO, failMessages)
 import Curry.Base.Position as CP
-import Curry.Base.Monad (runCYIO, failMessages)
 import qualified Curry.Syntax as CS
 import qualified Base.Types as CT
 import qualified CurryDeps as CD
@@ -12,9 +13,11 @@ import qualified CompilerOpts as CO
 import Modules (loadAndCheckModule)
 import qualified Text.PrettyPrint as PP
 
+import Control.Monad.Reader
 import qualified Data.Map as M
 import Data.Maybe
 import qualified Language.Haskell.LSP.Types as J
+import qualified Language.Haskell.LSP.Utility as U
 
 -- TODO: CT.PredType has been renamed to CT.Type in newer versions of curry-frontend
 data CompilationOutput = CompilationOutput { compilerEnv :: CE.CompilerEnv, moduleAST :: CS.Module (CT.PredType) }
@@ -26,15 +29,23 @@ type CompilationResult = Either [CM.Message] (CompilationOutput, [CM.Message])
 -- warning messages.
 compileCurry :: [String] -> FilePath -> IO CompilationResult
 compileCurry importPaths filePath = runCYIO $ do
-        srcs <- CD.flatDeps opts filePath
-        case srcs of
-            ((mdl, _):_) -> toCompilationOutput <$> loadAndCheckModule opts mdl filePath
-            _ -> failMessages [CM.message $ PP.text "Curry Language Server: Could not find module using flatDeps"]
+    deps <- CD.flatDeps opts filePath
+    liftIO $ U.logs $ "Compiling Curry, found deps: " ++ show deps
+    mdl <- justOrFail "Language Server: No module found" $ listToMaybe $ map fst $ filter (depMatches filePath . snd) $ deps
+    toCompilationOutput <$> loadAndCheckModule opts mdl filePath
     where cppOpts = CO.optCppOpts CO.defaultOptions
           cppDefs = M.insert "__PAKCS__" 300 (CO.cppDefinitions cppOpts)
           opts = CO.defaultOptions { CO.optForce = True,
                                      CO.optImportPaths = importPaths,
                                      CO.optCppOpts = cppOpts { CO.cppDefinitions = cppDefs } }
+
+depMatches :: FilePath -> CD.Source -> Bool
+depMatches fp1 (CD.Source fp2 _ _) = fp1 == fp2
+depMatches _ _ = False
+
+justOrFail :: String -> Maybe a -> CYIO a
+justOrFail _ (Just x) = return x
+justOrFail msg Nothing = failMessages [CM.message $ PP.text msg]
 
 -- TODO: CT.PredType has been renamed to CT.Type in newer versions of curry-frontend
 toCompilationOutput :: CE.CompEnv (CS.Module (CT.PredType)) -> CompilationOutput
