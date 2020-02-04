03d06c8a5feac4c2d889614631ba20460ded1593
fwcd <fwcdmail@gmail.com>
2020-02-05 00:35:56 +0100

    Fix rangeElem check

--- src/Curry/LanguageServer/Utils/General.hs
+++ src/Curry/LanguageServer/Utils/General.hs
@@ -12,8 +12,8 @@ lastSafe xs | null xs = Nothing
 
 -- | Tests whether a position is inside a given range.
 rangeElem :: J.Position -> J.Range -> Bool
-rangeElem (J.Position l c) range | l1 == l2  = c1 <= c && c <= c2
-                                 | l == l1   = c1 <= c
-                                 | l == l2   = c <= c2
-                                 | otherwise = l1 <= l && l <= l2
+rangeElem (J.Position l c) range = if l1 == l2 && l == l1 then c1 <= c && c <= c2
+                                   else if l == l1 then c1 <= c
+                                   else if l == l2 then c <= c2
+                                   else l1 <= l && l <= l2
     where (J.Range (J.Position l1 c1) (J.Position l2 c2)) = range
