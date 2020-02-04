df6faf2e1f3fa5e0f667662abde772bc3daa5185
fwcd <fwcdmail@gmail.com>
2020-02-04 21:37:25 +0100

    Shorter hover messages

--- src/Curry/LanguageServer/Features/Hover.hs
+++ src/Curry/LanguageServer/Features/Hover.hs
@@ -9,18 +9,19 @@ import Curry.LanguageServer.Compiler
 import Curry.LanguageServer.Utils.Conversions
 import Curry.LanguageServer.Utils.General
 import Curry.LanguageServer.Utils.Syntax
-import Data.Either.Extra
+import Data.Either.Extra (eitherToMaybe)
+import Data.Maybe (listToMaybe)
 import qualified Data.Text as T
 import qualified Language.Haskell.LSP.Types as J
 import qualified Language.Haskell.LSP.Utility as U
 
 fetchHover :: Show a => CompilationResult a -> J.Position -> IO (Maybe J.Hover)
 fetchHover compilation pos = do
-    let hover = toHover <$> (expressionAt pos =<< (moduleAST <$> fst <$> eitherToMaybe compilation))
+    let hover = toHover =<< expressionAt pos =<< (moduleAST <$> fst <$> eitherToMaybe compilation)
     U.logs $ "fetchHover: Found " ++ show hover
     return hover
 
-toHover :: Show a => CS.Expression a -> J.Hover
-toHover e = J.Hover msg range
+toHover :: Show a => CS.Expression a -> Maybe J.Hover
+toHover e = (flip J.Hover range) <$> msg
     where range = currySpanInfo2Range $ CSPI.getSpanInfo e
-          msg = J.HoverContents $ J.markedUpContent "curry" $ T.pack $ show e
+          msg = J.HoverContents <$> J.markedUpContent "curry" <$> T.pack <$> (listToMaybe $ words $ show e)
