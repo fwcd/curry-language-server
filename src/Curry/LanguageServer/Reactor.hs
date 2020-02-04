c20eb542708e6419e3cb4ea11a31db889540213c
fwcd <fwcdmail@gmail.com>
2020-02-04 21:32:20 +0100

    Implement hover request handler

--- src/Curry/LanguageServer/Reactor.hs
+++ src/Curry/LanguageServer/Reactor.hs
@@ -9,6 +9,7 @@ import Curry.LanguageServer.Aliases
 import Curry.LanguageServer.Compiler
 import qualified Curry.LanguageServer.Config as C
 import Curry.LanguageServer.Features.Diagnostics
+import Curry.LanguageServer.Features.Hover
 import Data.Default
 import qualified Language.Haskell.LSP.Core as Core
 import Language.Haskell.LSP.Diagnostics
@@ -50,6 +51,14 @@ reactor lf rin = do
                 compilation <- liftIO $ compileCurryFromUri config uri
                 diags <- liftIO $ fetchDiagnostics compilation
                 sendDiagnostics 100 uri version $ partitionBySource diags
+                
+            HandlerRequest (ReqHover req) -> do
+                liftIO $ U.logs $ "reactor: Processing ReqHover"
+                let uri = req ^. J.params . J.textDocument . J.uri
+                    pos = req ^. J.params . J.position
+                compilation <- liftIO $ compileCurryFromUri config uri
+                hover <- liftIO $ fetchHover compilation pos
+                send $ RspHover $ Core.makeResponseMessage req hover
 
             HandlerRequest req -> do
                 liftIO $ U.logs $ "reactor: Other HandlerRequest " ++ show req
@@ -59,6 +68,11 @@ compileCurryFromUri config uri = maybe failed (compileCurry importPaths) optFile
     where importPaths = C.importPaths config
           optFilePath = J.uriToFilePath uri
           failed = return $ failedCompilation "Language Server: Cannot construct file path from URI"
+
+send :: FromServerMessage -> RM ()
+send msg = do
+    lf <- ask
+    liftIO $ Core.sendFunc lf msg
 
 sendDiagnostics :: Int -> J.Uri -> J.TextDocumentVersion -> DiagnosticsBySource -> RM ()
 sendDiagnostics maxToPublish uri v diags = do
