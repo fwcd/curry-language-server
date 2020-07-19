48176db8994f04bcf9f980fab8e6d74385403498
fwcd <fwcdmail@gmail.com>
2020-07-20 01:35:25 +0200

    Catch exception fully in invokeCPM

--- src/Curry/LanguageServer/CPM/Process.hs
+++ src/Curry/LanguageServer/CPM/Process.hs
@@ -7,6 +7,7 @@ import Control.Exception (try, SomeException)
 import Control.Monad (when, fail)
 import Control.Monad.IO.Class (liftIO)
 import Curry.LanguageServer.CPM.Monad
+import Data.Either.Combinators (mapLeft)
 import System.Exit (ExitCode (..))
 import System.FilePath (FilePath)
 import System.Process
@@ -15,10 +16,9 @@ import System.Process
 invokeCPM :: FilePath -> [String] -> CM String
 invokeCPM dir args = do
     let executableName = "cypm"
-    res <- liftIO $ try $ readCreateProcessWithExitCode (proc executableName args) { cwd = Just dir } ""
-
-    case res of
-        Left e -> fail $ show (e :: SomeException) <> " (Please make sure that '" <> executableName <> "' is on your PATH!)"
-        Right (exitCode, out, err) -> do
-            when (exitCode /= ExitSuccess) $ fail err
-            return out
+        errMessage = "Please make sure that '" <> executableName <> "' is on your PATH!"
+        procOpts = (proc executableName args) { cwd = Just dir }
+    
+    (exitCode, out, err) <- cm $ mapLeft (const errMessage) <$> ((try $ readCreateProcessWithExitCode procOpts "") :: IO (Either SomeException (ExitCode, String, String)))
+    when (exitCode /= ExitSuccess) $ fail err
+    return out
