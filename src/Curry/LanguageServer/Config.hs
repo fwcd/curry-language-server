921bb896f0b114629da365282aaca35093d33175
fwcd <fwcdmail@gmail.com>
2021-04-15 18:46:16 +0200

    Add config flag for using snippet completions

--- src/Curry/LanguageServer/Config.hs
+++ src/Curry/LanguageServer/Config.hs
@@ -9,6 +9,7 @@ data Config = Config { cfgForceRecompilation :: Bool
                      , cfgLibraryPaths :: [FilePath]
                      , cfgLogLevel :: String
                      , cfgCurryPath :: String
+                     , cfgUseSnippetCompletions :: Bool
                      }
     deriving (Show, Eq)
 
@@ -18,25 +19,32 @@ instance Default Config where
                  , cfgLibraryPaths = []
                  , cfgLogLevel = "info"
                  , cfgCurryPath = "pakcs"
+                 , cfgUseSnippetCompletions = False
                  }
 
 instance FromJSON Config where
     parseJSON = withObject "Config" $ \o -> do
         c <- o .: "curry"
         l <- c .: "languageServer"
-        cfgForceRecompilation <- l .:? "forceRecompilation" .!= cfgForceRecompilation def
-        cfgImportPaths <- l .:? "importPaths" .!= cfgImportPaths def
-        cfgLibraryPaths <- l .:? "libraryPaths" .!= cfgLibraryPaths def
-        cfgLogLevel <- l .:? "logLevel" .!= cfgLogLevel def
-        cfgCurryPath <- l .:? "CurryPath" .!= cfgCurryPath def
+        cfgForceRecompilation    <- l .:? "forceRecompilation"    .!= cfgForceRecompilation def
+        cfgImportPaths           <- l .:? "importPaths"           .!= cfgImportPaths def
+        cfgLibraryPaths          <- l .:? "libraryPaths"          .!= cfgLibraryPaths def
+        cfgLogLevel              <- l .:? "logLevel"              .!= cfgLogLevel def
+        cfgCurryPath             <- l .:? "curryPath"             .!= cfgCurryPath def
+        cfgUseSnippetCompletions <- l .:? "useSnippetCompletions" .!= cfgUseSnippetCompletions def
         return Config {..}
 
 instance ToJSON Config where
-    toJSON Config {..} = object ["curry" .= object [ "languageServer" .= object [
-            "forceRecompilation" .= cfgForceRecompilation,
-            "importPaths" .= cfgImportPaths,
-            "libraryPaths" .= cfgLibraryPaths,
-            "logLevel" .= cfgLogLevel,
-            "CurryPath" .= cfgCurryPath
-        ]]]
+    toJSON Config {..} = object
+        ["curry" .= object
+            [ "languageServer" .= object
+                [ "forceRecompilation"    .= cfgForceRecompilation
+                , "importPaths"           .= cfgImportPaths
+                , "libraryPaths"          .= cfgLibraryPaths
+                , "logLevel"              .= cfgLogLevel
+                , "curryPath"             .= cfgCurryPath
+                , "useSnippetCompletions" .= cfgUseSnippetCompletions
+                ]
+            ]
+        ]
         
