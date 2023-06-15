d9092af6281e692df9253537b73eaa9464936756
fwcd <fwcdmail@gmail.com>
2023-06-16 00:35:16 +0200

    Remove unnecessary catch

--- src/Curry/LanguageServer/CPM/Process.hs
+++ src/Curry/LanguageServer/CPM/Process.hs
@@ -4,12 +4,10 @@ module Curry.LanguageServer.CPM.Process
     ( invokeCPM
     ) where
 
-import Control.Exception (try, IOException)
-import Control.Monad (when, join)
+import Control.Monad (when)
 import Control.Monad.Error.Class (MonadError(..))
-import Curry.LanguageServer.CPM.Monad (cpmm, runCPMM, CPMM)
+import Curry.LanguageServer.CPM.Monad (cpmm, CPMM)
 import Curry.LanguageServer.Utils.General (replaceString)
-import Data.Either.Combinators (mapLeft)
 import Data.Either.Extra (maybeToEither)
 import System.Exit (ExitCode (..))
 import System.Process (readCreateProcessWithExitCode, shell, CreateProcess(cwd))
@@ -17,11 +15,11 @@ import System.Timeout (timeout)
 
 -- | Invokes the Curry Package Manager executable with the specified args.
 invokeCPM :: FilePath -> [String] -> FilePath -> CPMM String
-invokeCPM dir args cpmPath = cpmm $ fmap (join . mapLeft errMessage) $ (try :: IO a -> IO (Either IOException a)) $ runCPMM $ do
+invokeCPM dir args cpmPath = do
     let action = readCreateProcessWithExitCode procOpts ""
 
     (exitCode, out, err) <- cpmm $ maybeToEither "CPM timed out!" <$> timeout microsecs action
-    when (exitCode /= ExitSuccess) $ throwError err
+    when (exitCode /= ExitSuccess) $ throwError $ errMessage err
 
     return out
     where errMessage e = "Please make sure that '" <> cpmPath <> "' exists or is on your PATH! Error: " ++ replaceString "\n" " " (show e)
