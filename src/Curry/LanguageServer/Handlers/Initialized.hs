eae0c314bf887350766d2b507fa66ec43bc7dec0
fwcd <fwcd@fwcd.dev>
2024-10-14 04:12:39 +0200

    Asynchronously build index store

--- src/Curry/LanguageServer/Handlers/Initialized.hs
+++ src/Curry/LanguageServer/Handlers/Initialized.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Curry.LanguageServer.Handlers.Initialized (initializedHandler) where
 
-import Control.Monad (void, forM_)
+import Control.Concurrent (forkIO)
+import Control.Monad (forM_, void)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.IO.Unlift (askRunInIO)
 import Curry.LanguageServer.FileLoader (fileLoader)
 import Curry.LanguageServer.Handlers.Diagnostics (emitDiagnostics)
 import Curry.LanguageServer.Utils.Logging (infoM)
@@ -16,21 +19,23 @@ import System.FilePath (takeFileName)
 
 initializedHandler :: S.Handlers LSM
 initializedHandler = S.notificationHandler J.SMethod_Initialized $ \_nt -> do
-    infoM "Building index store..."
-    workspaceFolders <- fromMaybe [] <$> S.getWorkspaceFolders
-    let folders = maybeToList . folderToPath =<< workspaceFolders
-        folderCount = length folders
+    runInIO <- askRunInIO
+    void $ liftIO $ forkIO $ runInIO $ S.withProgress "Curry: Adding folders" Nothing S.NotCancellable $ \update -> do
+        infoM "Building index store..."
+        workspaceFolders <- fromMaybe [] <$> S.getWorkspaceFolders
 
-    void $ S.withProgress "Curry: Adding folders" Nothing S.NotCancellable $ \update ->
+        let folders = maybeToList . folderToPath =<< workspaceFolders
+            folderCount = length folders
+
         forM_ (zip [0..] folders) $ \(i, fp) -> do
             let percent = (i * 100) `div` folderCount
                 msg = T.pack $ "[" ++ show (i + 1) ++ " of " ++ show folderCount ++ "] " ++ takeFileName fp
             update (S.ProgressAmount (Just $ fromIntegral percent) $ Just msg)
             addDirToIndexStore fp
 
-    count <- I.getModuleCount
-    infoM $ "Indexed " <> T.pack (show count) <> " files"
-    where folderToPath (J.WorkspaceFolder uri _) = J.uriToFilePath uri
+        count <- I.getModuleCount
+        infoM $ "Indexed " <> T.pack (show count) <> " files"
+        where folderToPath (J.WorkspaceFolder uri _) = J.uriToFilePath uri
 
 -- | Indexes a workspace folder recursively.
 addDirToIndexStore :: FilePath -> LSM ()
